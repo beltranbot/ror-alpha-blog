8706a53bfba76a004c525f81716cf96bc1971e56
beltranbot <beltranbot@gmail.com>
2021-05-19 21:34:40 -0500

    show validation errors and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,14 +8,19 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     # render plain: params[:article] # renders to browser
     @article = Article.new(params.require(:article).permit(:title, :description))
     # render plain: @article.inspect
-    @article.save
-    redirect_to article_path(@article) # extracts id from article and uses it for the redirect
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article) # extracts id from article and uses it for the redirect
+    else
+      render "new"
+    end
     # redirect_to @article # same functionality as the line above
   end
 end
