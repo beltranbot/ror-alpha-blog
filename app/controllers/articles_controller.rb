5780a250541c252df968637cdbfab4d1c50fe79a
beltranbot <beltranbot@gmail.com>
2021-05-19 21:23:59 -0500

    create article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    # render plain: params[:article] # renders to browser
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    @article.save
+    redirect_to article_path(@article) # extracts id from article and uses it for the redirect
+    # redirect_to @article # same functionality as the line above
   end
 end
