225cfeb0001ac4a43368d8fa13923c4877c9c35a
beltranbot <beltranbot@gmail.com>
2021-05-21 21:42:08 -0500

    refactorig and dry code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,12 +13,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # render plain: params[:article] # renders to browser
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params) # article_params is a method, find definition below
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params) # article_params is a method, find definition below
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
@@ -39,8 +39,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path # redirects to index
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
